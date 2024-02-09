239b38950396442830b79fa4931cdeaa4753d99d
Antoine Marras <57460188+spineki@users.noreply.github.com>
2024-02-09 15:39:36 +0100

    Update compile_tests.py
    Signed-off-by: Antoine Marras <57460188+spineki@users.noreply.github.com>

--- compile_tests.py
+++ compile_tests.py
@@ -7,23 +7,20 @@ import sys
 import re
 import os
 
-
 def make_tests(rv_tests_dir, dest_dir):
-    # print(rv_tests_dir)
-    # print(dest_dir)
     files = [f for f in os.listdir(rv_tests_dir) 
             if (os.path.isfile(os.path.join(rv_tests_dir, f)) 
                 & (".dump" not in f)
                 & ("Make" not in f)
                 & ("git" not in f)
                 )]
-    # print(files)
 
     for f in files:
         f = os.path.join(rv_tests_dir, f)
         filename = f.split("/")[-1].split("-")[-1]
+        print("------------------\n")
         print(filename, end="\n")
-        cmd = "riscv64-unknown-elf-objcopy -O binary " + f + " " + dest_dir + filename + ".bin"
+        cmd = "riscv64-unknown-elf-objcopy -O binary " + f + " " + dest_dir + "/" + filename + ".bin"
         result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         print(filename, result)
     print(os.listdir(dest_dir))
